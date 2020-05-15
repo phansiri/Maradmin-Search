3b078a7745f38ca8cde0220dfb23b0d122f56065
Lit Phansiri <phansiri@uw.edu>
2020-05-16 08:58:19 +0900

    initial celery commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
