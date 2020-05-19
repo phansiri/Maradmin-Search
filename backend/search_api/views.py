3eee354bc93d5f9d4e4b9d16c4ee8b72190a3410
Lit Phansiri <phansiri@uw.edu>
2020-05-19 23:19:25 +0900

    add pagination and filtering

--- backend/search_api/views.py
+++ backend/search_api/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import MaradminSerializer
 from .models import Maradmin
+from .filters import MaradminFilter
+from django_filters import rest_framework as filters
+from rest_framework.filters import OrderingFilter
+
+
 
 
 class MaradminViewSet(viewsets.ModelViewSet):
-    queryset = Maradmin.objects.all().order_by("-date")[:25]
+    queryset = Maradmin.objects.all()
     serializer_class = MaradminSerializer
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = MaradminFilter
+    ordering_fields = [
+        ("title", "title"),
+        ("date", "date"),
+        ("number", "number"),
+    ]
+    ordering = ["-date"]
