5b43b83abfec3095dfb62b4aa8222c04cabc3122
Litthideth Phansiri <lit.phansiri@friends.dds.mil>
2020-05-22 18:43:52 +0900

    add url model and update scrape

--- backend/search_api/models.py
+++ backend/search_api/models.py
@@ -6,6 +6,7 @@ class Maradmin(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     status = models.CharField(max_length=20)
+    body_link = models.URLField(blank=True)
 
     def __str__(self):
         return self.number
